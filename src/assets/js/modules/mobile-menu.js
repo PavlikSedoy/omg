d2827319135c306fef329a6884c34b3db17d70ff
PavelSedoy <pavliksedoy@gmail.com>
2020-04-17 02:49:58 +0300

    Animate navbar on scroll

--- src/assets/js/modules/mobile-menu.js
+++ src/assets/js/modules/mobile-menu.js
@@ -1,3 +1,4 @@
+// Mobile menu open/close
 import { disablePageScroll, enablePageScroll } from 'scroll-lock'
 
 const hamburgerCheck = document.getElementById('hamburger'),
@@ -18,3 +19,49 @@ hamburgerCheck.addEventListener('change', () => {
     // Change mobile nav state
     mobileMenuState = !mobileMenuState;
 });
+// End mobile menu open/close
+
+// Fixed/unfixed navbar
+import * as ScrollMagic from "scrollmagic";
+import { TweenMax, TimelineMax } from "gsap";
+import { ScrollMagicPluginGsap } from "scrollmagic-plugin-gsap";
+
+ScrollMagicPluginGsap(ScrollMagic, TweenMax, TimelineMax);
+
+const tl = new TimelineMax();
+
+const navbar = document.getElementById('navbar');
+let navbarActiveFlag = false;
+
+document.addEventListener('scroll', () => {
+    let scrollPos = window.pageYOffset;
+
+    if (scrollPos > 100 && !navbarActiveFlag) fixNavbar();
+    else if (scrollPos <= 100 && navbarActiveFlag) unfixNavbar();
+});
+
+// Fix navbar function
+const fixNavbar = () => {
+    // console.log('fix');
+    tl.fromTo(navbar, .5, {
+        opacity: 0
+    }, {
+        opacity: 1,
+        position: 'fixed',
+        backgroundColor: '#ffffff',
+        boxShadow: '0px 4px 74px rgba(0, 0, 0, 0.17)'
+});
+
+    navbarActiveFlag = true;
+}
+
+const unfixNavbar = () => {
+    tl.to(navbar, .5, {
+        position: 'absolute',
+        boxShadow: 'none',
+        backgroundColor: 'transparent'
+    });
+
+    navbarActiveFlag = false;
+}
+// End Fixed/unfixed navbar
