458501c625114a9cc7121b3e36477e3e498f6df2
PavelSedoy <pavliksedoy@gmail.com>
2020-04-17 04:34:43 +0300

    For who desktop

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -10,6 +10,11 @@ import '@images/header/bg.svg';
 import '@images/header/teacher.jpg';
 import '@svg/dot.svg';
 import '@svg/arrow-bottom.svg';
+import "@images/section-title-image.png";
+import "@images/for-who/face-spa.jpg";
+import "@images/for-who/list.jpg";
+import "@images/for-who/item-dots.png";
+import "@images/for-who/bg.jpg";
 
 // Scripts
 import '@js/modules/mobile-menu';
