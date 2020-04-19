935a2a7b3b57199f68a513cf89ab744705499630
PavelSedoy <pavliksedoy@gmail.com>
2020-04-19 15:22:11 +0300

    Gallery section

--- src/assets/js/modules/galleries.js
+++ src/assets/js/modules/galleries.js
@@ -2,20 +2,18 @@ require('lightgallery.js/dist/js/lightgallery');
 require('lg-video.js/dist/lg-video');
 
 lightGallery(document.getElementById('reviewsMessageGallery'), {
-    thumbnail: false,
+    // thumbnail: false,
     selector: '.msg-gallery__link'
 });
 lightGallery(document.getElementById('reviewsPhotoGallery'), {
-    thumbnail: false,
+    // thumbnail: false,
     selector: '.msg-gallery__link--photo1'
 });
-lightGallery(document.getElementById('reviewsVideoGallery'), {
+lightGallery(document.getElementById('galleryPhotoGallery'), {
     // thumbnail: false,
-    selector: '.msg-gallery__link--video1',
-    youtubePlayerParams: {
-        modestbranding: 1,
-        showinfo: 0,
-        rel: 0,
-        controls: 0
-    },
+    selector: '.msg-gallery__link--photo2'
+});
+lightGallery(document.getElementById('galleryVideoGallery'), {
+    // thumbnail: false,
+    selector: '.msg-gallery__link--video2',
 });
--- src/assets/js/modules/tabs.js
+++ src/assets/js/modules/tabs.js
@@ -1,4 +1,6 @@
 const tabs = require ('tabs/index');
 const reviewsTabs = document.getElementById('reviewsTabs');
+const galleryTabs = document.getElementById('galleryTabs');
 
 tabs(reviewsTabs);
+tabs(galleryTabs);
